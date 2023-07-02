c44f4c76478470ca965374fdc4dcab17cca449e5
Jérémie Bellavance <jbellavance34@gmail.com>
2023-07-01 23:17:20 -0400

    apigw + dynamodb

--- lib/mayoche-stack.ts
+++ lib/mayoche-stack.ts
@@ -1,12 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as s3_deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 
 export class MayocheFrontendStack extends cdk.Stack {
@@ -80,12 +85,6 @@ export class MayocheFrontendStack extends cdk.Stack {
       allowedMethods: [s3.HttpMethods.GET],
       allowedHeaders: ['*']
     })
-   // const lambda_function = new lambda.Function(this, 'mayoche-fetch-data', {
-   //   runtime: cdk.aws_lambda.Runtime.PYTHON_3_10,
-   //   code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda/image_scraper')),
-   //   handler: 'index.handler',
-   // })
-   // TODO : Add cloudwatch event with crontab
     const lambda_fetch_data = new lambda.DockerImageFunction(this, 'mayoche-fetch-data-docker', {
       code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, 'lambda/image_scraper')),
       environment: {
@@ -95,6 +94,14 @@ export class MayocheFrontendStack extends cdk.Stack {
       memorySize: 256,
       reservedConcurrentExecutions: 1,
     });
+    // cloudwatch event rule to trigger lambda_fetch_data once every day at 3 am
+    new events.Rule(this, 'mayoche-fetch-data-rule', {
+      schedule: events.Schedule.expression('cron(0 3 * * ? *)'),
+      targets: [
+        new targets.LambdaFunction(lambda_fetch_data)
+      ]
+    })
+
     s3_data_bucket.grantWrite(lambda_fetch_data);
     // adding behavior to /image with s3_data_bucket
     cloudfrontDistribution.addBehavior('data/*.json', new origins.S3Origin(s3_data_bucket), {
@@ -106,6 +113,153 @@ export class MayocheFrontendStack extends cdk.Stack {
           minTtl: cdk.Duration.minutes(1)
       })
     })
+    const modelName = 'choice'
+    // dynamoDB to store data
+    const dynamoTable = new dynamodb.Table(this, modelName, {
+          billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+          partitionKey: {
+            name: `${modelName}Id`,
+            type: dynamodb.AttributeType.STRING,
+          },
+          removalPolicy: cdk.RemovalPolicy.RETAIN,
+          tableName: modelName,
+    });
+    const api = new apigw.RestApi(this, 'mayoche-data-api', { 
+      restApiName: 'mayoche-data-api',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+      },
+    })
+    new apigw.UsagePlan(this, 'mayoche-data-api-usage-plan', {
+      name: 'mayoche-data-api-usage-plan',
+      apiStages: [{
+        api: api,
+        stage: api.deploymentStage,
+      }],
+      throttle: {
+        burstLimit: 100,
+        rateLimit: 200,
+      }
+    })
+    const allResources = api.root.addResource(modelName.toLocaleLowerCase());
+    const oneResource = allResources.addResource('{id}');
+    const getPolicy = new iam.Policy(this, 'getPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['dynamodb:GetItem'],
+          effect: iam.Effect.ALLOW,
+          resources: [dynamoTable.tableArn],
+        }),
+      ],
+    });
+    const getRole = new iam.Role(this, 'getRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+    getRole.attachInlinePolicy(getPolicy);
+    const errorResponses = [
+      {
+        selectionPattern: '400',
+        statusCode: '400',
+        responseTemplates: {
+          'application/json': `{
+            "error": "Bad input!"
+          }`,
+        },
+      },
+      {
+        selectionPattern: '5\\d{2}',
+        statusCode: '500',
+        responseTemplates: {
+          'application/json': `{
+            "error": "Internal Service Error!"
+          }`,
+        },
+      },
+    ];
 
+    const integrationResponses = [
+      {
+        statusCode: '200',
+      },
+      ...errorResponses,
+    ];
+    const getIntegration = new apigw.AwsIntegration({
+      action: 'GetItem',
+      options: {
+        credentialsRole: getRole,
+        integrationResponses,
+        requestTemplates: {
+          'application/json': `{
+              "Key": {
+                "${modelName}Id": {
+                  "S": "$method.request.path.id"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    });
+    const putPolicy = new iam.Policy(this, 'putPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['dynamodb:PutItem'],
+          effect: iam.Effect.ALLOW,
+          resources: [dynamoTable.tableArn],
+        }),
+      ],
+    });
+    const putRole = new  iam.Role(this, 'putRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+    putRole.attachInlinePolicy(putPolicy);
+    const createIntegration = new apigw.AwsIntegration({
+      action: 'PutItem',
+      options: {
+        credentialsRole: putRole,
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseTemplates: {
+              'application/json': `{
+                "requestId": "$context.requestId"
+              }`,
+            },
+          },
+          ...errorResponses,
+        ],
+        requestTemplates: {
+          'application/json': `{
+              "Item": {
+                "${modelName}Id": {
+                  "S": "$context.requestId"
+                },
+                "Animal": {
+                  "S": "$input.path('$.animal')"
+                },
+                "CreatedAt": {
+                  "S": "$context.requestTime"
+                },
+                "Description": {
+                  "S": "$input.path('$.description')"
+                },
+                "Name": {
+                  "S": "$input.path('$.name')"
+                },
+                "type": {
+                  "S": "vote"
+                }
+              },
+              "TableName": "${modelName}"
+            }`,
+        },
+      },
+      service: 'dynamodb',
+    });
+    const methodOptions = { methodResponses: [{ statusCode: '200' }, { statusCode: '400' }, { statusCode: '500' }] };
+
+    allResources.addMethod('POST', createIntegration, methodOptions);
+    oneResource.addMethod('GET', getIntegration, methodOptions);
   }
 }
